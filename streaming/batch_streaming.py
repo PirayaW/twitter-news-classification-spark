35c38b7073ab1e0b6974672893054caa7b97841a
Punit Shetty <punitshetty30@gmail.com>
2016-05-22 17:32:30 -0700

    Splitting files into train and test

--- streaming/batch_streaming.py
+++ streaming/batch_streaming.py
@@ -3,6 +3,9 @@ import os
 import shutil
 import time
 
+from Tools.scripts.treesync import raw_input
+from random import sample
+
 python_version = sys.version_info.major
 if python_version == 3:
     import configparser
@@ -19,6 +22,7 @@ if __name__ == '__main__':
 
     path = config.get('Path', 'batch_data')
     dest = config.get('Path', 'streaming_data')
+    test_dest = config.get('Path', 'streaming_test_data')
 
     # list of files
     filenames = next(os.walk(path))[2]
@@ -26,6 +30,9 @@ if __name__ == '__main__':
     if not os.path.exists(dest):
         os.makedirs(dest)
     destfiles = next(os.walk(dest))[2]
+    if not os.path.exists(test_dest):
+        os.makedirs(test_dest)
+    desttestfiles = next(os.walk(test_dest))[2]
 
     if len(destfiles) > 0:
         if python_version == 3:
@@ -35,11 +42,23 @@ if __name__ == '__main__':
         if delete == 'y' or delete == 'Y':
             shutil.rmtree(dest, ignore_errors=True)
             os.makedirs(dest)
+            shutil.rmtree(test_dest, ignore_errors=True)
+            os.makedirs(test_dest)
         else:
             print("Terminating")
             exit(0)
 
-    for file in filenames:
-        shutil.copyfile(path + file, dest + file)
-        print('Copied: ' + file)
-        time.sleep(frequency)
+    tr_percent = 0.7
+    tr_numfiles = round(tr_percent*len(filenames))
+    tr_num  = sample(range(len(filenames)), tr_numfiles)
+
+    for i in range(0,len(filenames)):
+        file = filenames[i]
+        if i in tr_num or i == 0:
+            shutil.copyfile(path + file, dest + file)
+            print('Copied Training file: ' + file)
+            time.sleep(frequency)
+        else:
+            shutil.copyfile(path + file, test_dest + file)
+            print('Copied Testing file: ' + file)
+            time.sleep(frequency)
