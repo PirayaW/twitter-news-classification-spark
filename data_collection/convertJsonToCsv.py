a521a63639d7d8c9a8edc8ce11c8f169a318af74
PirayaW <pirayaw@users.noreply.github.com>
2016-05-26 03:02:42 -0700

    Added more data
    politics (@bpolitics), finance (@ReutersBiz), and sports (@espn)

---
+++ data_collection/convertJsonToCsv.py
@@ -0,0 +1,45 @@
+import json
+import datetime
+import csv
+import sys
+import dateutil.parser as parser
+
+python_version = sys.version_info.major
+if python_version == 3:
+    import configparser
+else:
+    import ConfigParser as configparser
+
+
+def toDatetime(str):
+    return parser.parse(str)
+
+
+if __name__ == '__main__':
+    config = configparser.RawConfigParser()
+    config.read('../config.properties')
+    datapath = config.get('Path', 'json_data')
+    batchpath = config.get('Path', 'batch_data')
+
+    account = ['new_bpolitics',
+               'new_ReutersBiz',
+               'new_espn'
+               ]
+    label = [0,
+             1,
+             2
+             ]
+
+    file = open('additionalPoliticsFinanceSports.csv', 'w')
+    writer = csv.writer(file, delimiter='`', quotechar='|', quoting=csv.QUOTE_ALL)
+
+    for i in range(len(account)):
+        acc = account[i]
+        f = open(datapath + acc + '.json')
+        for line in f:
+            tweet = json.loads(line)
+            time = toDatetime(tweet['created_at'])
+            row = [tweet['text'].encode('ascii', 'replace'), label[i], time.strftime("%Y%m%d_%H"), acc]
+            writer.writerow(row)
+    f.close()
+
